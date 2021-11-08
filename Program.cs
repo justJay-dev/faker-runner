5f7cb7fe0933a9eb12b50b7f077b8d343243155d
Jay Simpson <jay_simpson@unigroup.com>
2021-11-08 13:35:43 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Faker.Datatype;
+namespace faker_runner
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("<- UUID ->");
+			Console.WriteLine(Faker.Datatype.Uuid.Next());
+			Console.WriteLine("<- BOOLEAN ->");
+			Console.WriteLine(Faker.Datatype.Boolean.Next());
+			Console.WriteLine("<- RANDOM INT->");
+			Console.WriteLine(Faker.Datatype.Int.Next(1, 69));
+			Console.WriteLine("<- RANDOM FLOAT->");
+			Console.WriteLine(Faker.Datatype.Float.Next(1, 69));
+			//broken
+			Console.WriteLine("<- RANDOM 7 CHAR STRING->");
+			Console.WriteLine(Faker.Datatype.String.Next(7));
+		}
+	}
+}
