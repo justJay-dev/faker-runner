9d8841c81887c2a7ced9e4de53ea7ffc90e03076
Jay Simpson <jay_simpson@unigroup.com>
2021-11-09 13:57:15 -0500

    moar stuff

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Faker.Datatype;
+using Faker;
 namespace faker_runner
 {
 	class Program
@@ -7,16 +7,44 @@ namespace faker_runner
 		static void Main(string[] args)
 		{
 			Console.WriteLine("<- UUID ->");
-			Console.WriteLine(Faker.Datatype.Uuid.Next());
+			Console.WriteLine(Faker.Datatype.Uuid());
 			Console.WriteLine("<- BOOLEAN ->");
-			Console.WriteLine(Faker.Datatype.Boolean.Next());
-			Console.WriteLine("<- RANDOM INT->");
-			Console.WriteLine(Faker.Datatype.Int.Next(1, 69));
-			Console.WriteLine("<- RANDOM FLOAT->");
-			Console.WriteLine(Faker.Datatype.Float.Next(1, 69));
-			//broken
+			Console.WriteLine(Faker.Datatype.Boolean());
+			Console.WriteLine("<- RANDOM INT BETWEEN 1 and 69->");
+			Console.WriteLine(Faker.Datatype.Int(1, 69));
+			Console.WriteLine("<- RANDOM INT UNDEFINED ->");
+			Console.WriteLine(Faker.Datatype.Int());
+			Console.WriteLine("<- RANDOM FLOAT BETWEEN 1 and 69->");
+			Console.WriteLine(Faker.Datatype.Double(1, 69));
+			Console.WriteLine("<- RANDOM FLOAT BETWEEN UNDEFINED");
+			Console.WriteLine(Faker.Datatype.Double());
 			Console.WriteLine("<- RANDOM 7 CHAR STRING->");
-			Console.WriteLine(Faker.Datatype.String.Next(7));
+			Console.WriteLine(Faker.Datatype.String(7));
+			Console.WriteLine("<- RANDOM UNDEFINED LENGTH STRING->");
+			Console.WriteLine(Faker.Datatype.String());
+			Console.WriteLine("<-- RANDOM STRING ARRAY OF 10-->");
+			var stringArray = Faker.Datatype.Array("string", 10);
+			foreach (var item in stringArray)
+			{
+				Console.WriteLine(item.ToString());
+			}
+			Console.WriteLine("<-- RANDOM INT ARRAY OF 10-->");
+			var intArray = Faker.Datatype.Array("int", 10);
+			foreach (var item in intArray)
+			{
+				Console.WriteLine(item.ToString());
+			}
+
+			Console.WriteLine("<-- RANDOM OBJECT-->");
+			Console.WriteLine(Faker.Datatype.Object());
+
+			Console.WriteLine("<-- HACKER-->");
+			Console.WriteLine(Faker.Hacker.Abbreviation());
+			Console.WriteLine(Faker.Hacker.Adjective());
+			Console.WriteLine(Faker.Hacker.IngVerb());
+			Console.WriteLine(Faker.Hacker.Noun());
+			Console.WriteLine(Faker.Hacker.Phrase());
+			Console.WriteLine(Faker.Hacker.Verb());
 		}
 	}
 }
