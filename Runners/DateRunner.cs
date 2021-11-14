7e45d3adddf85a208317931cd5d248c06495bece
Jay <jay@lowbrowlabs.com>
2021-11-13 22:01:10 -0500

    date runners

--- Runners/DateRunner.cs
+++ Runners/DateRunner.cs
@@ -3,19 +3,22 @@ using System;
 using Faker;
 namespace FakerRunner
 {
-	class DateRunner
-	{
-		public static void Run()
-		{
-			Console.WriteLine("<-- DATE -->");
-			Console.WriteLine("Month--> " + Faker.Date.Month());
-			Console.WriteLine("MonthAbbr--> " + Faker.Date.Month(true));
-			Console.WriteLine("Weekday--> " + Faker.Date.Weekday());
-			Console.WriteLine("Weekday Abbr--> " + Faker.Date.Weekday(true));
-			Console.WriteLine("Past--> " + Faker.Date.Past(69));
-			Console.WriteLine("Future--> " + Faker.Date.Future(2));
-			Console.WriteLine("<--/ DATE -->");
-			return;
-		}
-	}
+    class DateRunner
+    {
+        public static void Run()
+        {
+            Console.WriteLine("<-- DATE -->");
+            Console.WriteLine("Month--> " + Faker.Date.Month());
+            Console.WriteLine("MonthAbbr--> " + Faker.Date.Month(true));
+            Console.WriteLine("Weekday--> " + Faker.Date.Weekday());
+            Console.WriteLine("Weekday Abbr--> " + Faker.Date.Weekday(true));
+            Console.WriteLine("Past--> " + Faker.Date.Past(69));
+            Console.WriteLine("Future--> " + Faker.Date.Future(2));
+            Console.WriteLine("Between--> " + Faker.Date.Between("06/01/1990", "06/01/1991"));
+            Console.WriteLine("Recent--> " + Faker.Date.Recent(15, "06/01/1991"));
+            Console.WriteLine("Soon--> " + Faker.Date.Soon(15, "06/01/1991"));
+            Console.WriteLine("<--/ DATE -->");
+            return;
+        }
+    }
 }
